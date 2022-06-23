ba78573c6008fb1dc8d9240f529c50170c56e0db
Pamela <abrepamm@gmail.com>
2022-06-23 20:54:06 -0300

    rota / e componente login criado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="text" />
+        <button type="button">Entrar</button>
+      </div>);
   }
 }
 

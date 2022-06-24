f6b02b257a54edf02ee8dc297466e4de9af00a4e
Pamela <abrepamm@gmail.com>
2022-06-23 23:52:12 -0300

    ation addEmail criada

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import user from './user';
 import wallet from './wallet';
+import { ADD_EMAIL } from '../actions';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
@@ -11,6 +12,10 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+  case ADD_EMAIL:
+    return {...state, 
+      action.email,
+    };
   default:
     return state;
   }

7c2be75a4bb2e1755ec3962928ca31c1fff93cf5
Pamela <abrepamm@gmail.com>
2022-06-24 15:22:50 -0300

    lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { validacaoEmail } from '../funcs/validEmail';
+import validacaoEmail from '../funcs/validEmail';
 import { addEmail } from '../actions';
 
 class Login extends React.Component {

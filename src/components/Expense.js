6ef6cb92f9759aa38b608370f9b09f9b4e551df4
Pamela <abrepamm@gmail.com>
2022-06-27 18:52:23 -0300

    state de despesas criado, funções de change criada

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -3,48 +3,99 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Expense extends React.Component {
+  state = {
+    id: 0,
+    value: '',
+    description: '',
+    currency: 'USD',
+    method: 'Dinheio',
+    tag: 'Alimentação',
+    // exchangeRates: {},
+  }
+
+  handleOnChange = (e) => {
+    const { target: { value, name } } = e;
+    this.setState({ [name]: value });
+  }
+
+  handleOnClick = () => {
+    this.newID();
+  }
+
+  newID=() => {
+    const { id } = this.state;
+    this.setState({ id: id + 1 });
+  }
+
   render() {
     const { currencies } = this.props;
+    const { value, description, currency, method, tag } = this.state;
     return (
       <form>
         <label htmlFor="valor">
           Valor
           <input
             id="valor"
+            value={ value }
+            name="value"
             type="number"
             data-testid="value-input"
+            onChange={ this.handleOnChange }
           />
         </label>
         <label htmlFor="descrição">
           descrição
           <input
+            value={ description }
             id="descrição"
             type="text"
+            name="description"
             data-testid="description-input"
+            onChange={ this.handleOnChange }
           />
         </label>
         <label htmlFor="Moeda">
           moeda
           <select
+            value={ currency }
             id="Moeda"
+            name="currency"
+            onChange={ this.handleOnChange }
           >
             {currencies.map((element) => (
               <option key={ element }>{element}</option>
             ))}
           </select>
         </label>
-        <select data-testid="method-input">
+        <select
+          name="method"
+          value={ method }
+          data-testid="method-input"
+          onChange={ this.handleOnChange }
+        >
           <option>Dinheiro</option>
           <option>Cartão de crédito</option>
           <option>Cartão de débito</option>
         </select>
-        <select data-testid="tag-input">
+        <select
+          name="tag"
+          value={ tag }
+          onChange={ this.handleOnChange }
+          data-testid="tag-input"
+        >
           <option>Alimentação</option>
           <option>Trabalho</option>
           <option>Lazer</option>
           <option>Transporte</option>
           <option>Saúde</option>
         </select>
+
+        <button
+          type="button"
+          onClick={ this.handleOnClick }
+        >
+          Adicionar despesa
+        </button>
 
       </form>
     );
